bd794cb5a94465e613fecfc4ff37af1edcc77587
Sabuj <sabujlovescoding@gmail.com>
2019-08-27 18:33:08 +0530

    manthan codefest' 19

---
+++ Manthan Codefest IIT BHU/C.cpp
@@ -0,0 +1,11 @@
+    #include <bits/stdc++.h>
+     
+    using namespace std;
+     
+    
+     
+    int main() {
+       ios_base::sync_with_stdio(false);
+        freopen("input.txt", "r" , stdin);
+      
+    }
