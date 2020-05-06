02336a5bc1dc1a4610fc5aa030de849da081c37a
Sabuj Jana <sabujlovescoding@gmail.com>
2020-05-06 18:22:53 +0530

    stars + bars combinatorics

---
+++ SPOJ/Marbles.cpp
@@ -0,0 +1,62 @@
+/*--------------------------"SABUJ-JANA"------"JADAVPUR UNIVERSITY"--------*/
+/*-------------------------------@greenindia-----------------------------------*/
+//
+//		  _____       _           _        _
+//		 / ____|     | |         (_)      | |
+//		| (___   __ _| |__  _   _ _       | | __ _ _ __   __ _
+//		 \___ \ / _` | '_ \| | | | |  _   | |/ _` | '_ \ / _` |
+//		 ____) | (_| | |_) | |_| | | | |__| | (_| | | | | (_| |
+//		|_____/ \__,_|_.__/ \__,_| |  \____/ \__,_|_| |_|\__,_|
+//		                        _/ |
+//		                       |__/
+/*---------------------- Magic. Do not touch.-----------------------------*/
+/*------------------------------God is Great/\---------------------------------*/
+#include <bits/stdc++.h>
+using namespace std;
+#define crap ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+//cout<<fixed<<showpoint<<setprecision(12)<<ans<<endl;
+#define int long long int
+#define double long double
+#define PI acos(-1)
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+
+void print1d(const vector<int>& vec) {for (auto val : vec) {cout << val << " ";} cout << endl;}
+
+void print2d(const vector<vector<int>>& vec) {for (auto row : vec) {for (auto val : row) {cout << val << " ";} cout << endl;}}
+
+int nck(int n, int k) {
+	double res = 1;
+	for (int i = 1; i <= k; i++) {
+		res = res * (double)(n - k + i) / i;
+	}
+
+	return (int)(res + 0.01);
+}
+
+void solve() {
+	int n, k;
+	cin >> n >> k;
+
+	int N = (n - k) + k - 1;// stars + bars
+	int R = min(k - 1, N);
+
+	cout << nck(N, R) << endl;
+}
+
+signed main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("error.txt", "w", stderr);
+#endif
+	crap;
+
+	int t;
+	cin >> t;
+
+	while (t--) {
+		solve();
+	}
+
+	return 0;
+}
