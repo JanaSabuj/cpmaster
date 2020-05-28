75012cc4e5240a62c22c501a7005f0dc4d4fd62f
Sabuj Jana <sabujlovescoding@gmail.com>
2020-05-28 22:19:24 +0530

    Add files via upload

---
+++ Codeforces/CF Round 645 Div2/B.cpp
@@ -0,0 +1,66 @@
+/*--------------------------"SABUJ-JANA"------"JADAVPUR UNIVERSITY"--------*/
+/*-------------------------------@greenindia-----------------------------------*/
+//
+//		  _____       _           _        _
+//		 / ____|     | |         (_)      | |
+//		| (___   __ _| |__  _   _ _       | | __ _ _ __   __ _
+//		 \___ \ / _` | '_ \| | | | |  _   | |/ _` | '_ \ / _` |
+//		 ____) | (_| | |_) | |_| | | | |__| | (_| | | | | (_| |
+//		|_____/ \__,_|_.__/ \__,_| |  \____/ \__,_|_| |_|\__,_|
+//		                        _/ |
+//		                       |__/
+/*---------------------- Magic. Do not touch.-----------------------------*/
+/*------------------------------God is Great/\---------------------------------*/
+#include <bits/stdc++.h>
+using namespace std;
+#define crap ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+//cout<<fixed<<showpoint<<setprecision(12)<<ans<<endl;
+#define int long long int
+#define double long double
+#define PI acos(-1)
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+#define endl "\n"
+
+void print1d(const vector<int>& vec) {for (auto val : vec) {cout << val << " ";} cout << endl;}
+
+void print2d(const vector<vector<int>>& vec) {for (auto row : vec) {for (auto val : row) {cout << val << " ";} cout << endl;}}
+
+void solve() {
+	int n;
+	cin >> n;
+
+	vi arr(n);
+	for (int i = 0; i < n; ++i) {
+		cin >> arr[i];
+	}
+
+	sort(arr.begin(), arr.end());
+
+	for (int i = n - 1; i >= 0; i--) {
+		if (i + 1 >= arr[i]) {
+			cout << i + 2 << endl;
+			return;
+		}
+	}
+
+	cout << 1 << endl;
+}
+
+signed main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("error.txt", "w", stderr);
+#endif
+	crap;
+
+	int t;
+	cin >> t;
+
+	while (t--) {
+		solve();
+	}
+
+
+	return 0;
+}
