7f6ee997c583222504a256e3247840254a254352
sabuj.jana <sabuj.jana@flipkart.com>
2022-11-24 08:24:33 +0530

    cf div 4 in go

---
+++ Codeforces/Codeforces Round #835 (Div. 4)/A-Medium-Number.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+var t int
+
+func solve() {
+	vecs := [3]int{}
+	fmt.Scanln(&vecs[0], &vecs[1], &vecs[2])
+	sort.Ints(vecs[:])
+	fmt.Println(vecs[1])
+}
+
+func main() {
+	fmt.Scanln(&t)
+	for i := 0; i < t; i++ {
+		solve()
+	}
+}
