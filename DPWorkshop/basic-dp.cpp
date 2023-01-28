c250bd08c6e5b26f5aef7b5bdbbdf1083010c759
sabuj.jana <sabuj.jana@flipkart.com>
2023-01-28 22:24:26 +0530

    DPWorkshop added

--- DPWorkshop/basic-dp.cpp
+++ DPWorkshop/basic-dp.cpp
@@ -23,3 +23,17 @@
 	    In DP:
 	    - Time Complexity Decreases, Space Comp Increases
 
+	    Order of dp params need not be the same in the dp array:
+	    - dp(a, b, c) (0-9, 0-99, 0-999)
+	    - cache[b][a][c] = cache[100][10][1000]
+	    - just make sure to access in the correct order in the func
+	    - worst int arr size you can go to is 10^8, > 1e8 will give you MLE
+	    - in global scope, max size is 1e8 (bcoz created in heap)
+	    - in local scope, in main scope, max size is 2*1e6(bcoz created in stack)
+	    - https://stackoverflow.com/questions/32409910/why-on-declaring-an-array-global-in-c-the-size-that-it-can-be-given-is-larger
+	    - dp[less_sz][more_sz] >> dp[more_sz][less_sz] .e, dp[2][10] >> dp[10][2]
+	    - cache coherence problem
+	    - dp[10][100][1000], also dp[1000000] ,also dp[1000][1000]
+	    - a, b, c can be compressed to x = a*10^5 + b*10^3 + c, then later a,b,c can be retrieved from x
+
+
