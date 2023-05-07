d0cd5aa257091985bff0d5a91b0b9dc938d4c6e8
sabuj.jana <sabuj.jana@flipkart.com>
2023-05-07 12:02:59 +0530

    added cf template for contests

---
+++ templates/template.cpp
@@ -0,0 +1,33 @@
+/*-------------------------------@greenindia-----------------------------------*/
+/*------------------------------God is Great/\---------------------------------
+███████╗ █████╗ ██████╗ ██╗   ██╗     ██╗         ██╗ █████╗ ███╗   ██╗ █████╗
+██╔════╝██╔══██╗██╔══██╗██║   ██║     ██║         ██║██╔══██╗████╗  ██║██╔══██╗
+███████╗███████║██████╔╝██║   ██║     ██║         ██║███████║██╔██╗ ██║███████║
+╚════██║██╔══██║██╔══██╗██║   ██║██   ██║    ██   ██║██╔══██║██║╚██╗██║██╔══██║
+███████║██║  ██║██████╔╝╚██████╔╝╚█████╔╝    ╚█████╔╝██║  ██║██║ ╚████║██║  ██║
+╚══════╝╚═╝  ╚═╝╚═════╝  ╚═════╝  ╚════╝      ╚════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define crap                    \
+   ios::sync_with_stdio(false); \
+   cin.tie(NULL);               \
+   cout.tie(NULL)
+// cout<<fixed<<showpoint<<setprecision(12)<<ans<<endl;
+#define endl "\n"
+//  #define int long long int
+//  #define double long double
+
+signed main()
+{
+   freopen("input.txt", "r", stdin);
+   freopen("output.txt", "w", stdout);
+   crap;
+
+   int x;
+   cin >> x;
+   cout << x << endl;
+
+   return 0;
+}
